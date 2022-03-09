88f37a98338e851cfa6a1dad1df2d03ed4d4ed30
ghen-git <gabriele.garigliano@gmail.com>
2022-03-09 20:18:47 +0100

    roads load weird

--- Assets/Scripts/RoadGenerator.cs
+++ Assets/Scripts/RoadGenerator.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 
 public class Block
 {
-    public Vector3 topLeft;
-    public Vector3 topRight;
-    public Vector3 bottomLeft;
-    public Vector3 bottomRight;
+    public Vector2 topLeft;
+    public Vector2 topRight;
+    public Vector2 bottomLeft;
+    public Vector2 bottomRight;
+
+    public Block(Vector2 topLeft, Vector2 topRight, Vector2 bottomLeft, Vector2 bottomRight)
+    {
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
 }
 
 public class RoadGenerator : MonoBehaviour
 {
-    int renderDistance = 3;
-    float blockSize = 10f;
+    int renderDistance = 12;
+    float blockSize =100f;
+    float roadSize = 20f;
+    float variationAmount = 25f;
     Dictionary<Vector2Int, Block> worldMap = new Dictionary<Vector2Int, Block>();
     Transform player;
     GameObject housePrefab;
@@ -38,8 +48,14 @@ public class RoadGenerator : MonoBehaviour
         {
             LoadBlocks();
             UnloadBlocks();
+            LoadRoads();
             loadedBounds = bounds;
         }
+    }
+
+    void LoadRoads()
+    {
+
     }
 
     string VectToName(Vector2Int vect)
@@ -58,10 +74,155 @@ public class RoadGenerator : MonoBehaviour
             for(int y = bounds[2].y; y <= bounds[1].y; y++)
             {
                 Vector2Int block = new Vector2Int(x, y);
-                GameObject house = GameObject.Instantiate(housePrefab, new Vector3(block.x * blockSize, 0, block.y * blockSize), new Quaternion());
-                house.name = VectToName(block);
-                house.AddComponent<MeshCollider>();
+
+                if(worldMap.ContainsKey(block))
+                    RenderBlock(block);
+                else
+                {
+                    GenerateBlock(block);
+                    RenderBlock(block);
+                }
             }
+    }
+
+    void RenderBlock(Vector2Int coords)
+    {
+        Block blockPars = worldMap[coords];
+        GameObject block = new GameObject();
+
+        MeshRenderer meshRenderer = block.AddComponent<MeshRenderer>();
+        meshRenderer.material = Resources.Load<Material>("Materials/Ground/Bricks");
+
+        MeshFilter meshFilter = block.AddComponent<MeshFilter>();
+
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[]
+        {
+            //bottom quad
+            new Vector3(blockPars.topLeft.x, 0, blockPars.topLeft.y), //top left 0
+            new Vector3(blockPars.topRight.x, 0, blockPars.topRight.y), //top right 1 
+            new Vector3(blockPars.bottomLeft.x, 0, blockPars.bottomLeft.y), //bottom left 2 
+            new Vector3(blockPars.bottomRight.x, 0, blockPars.bottomRight.y) //bottom right 3
+        };
+        mesh.vertices = vertices;
+
+        int[] tris = new int[]
+        {
+            2, 0, 3,
+            0, 1, 3
+        };
+        mesh.triangles = tris;
+
+        Vector2[] uv = new Vector2[4]
+        {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(0, 1),
+            new Vector2(1, 1)
+        };
+        mesh.uv = uv;
+
+        meshFilter.mesh = mesh;
+        mesh.RecalculateTangents();
+        mesh.RecalculateNormals();
+
+        block.transform.position = new Vector3(coords.x * blockSize, 0, coords.y * blockSize);
+        block.name = VectToName(coords);
+        block.AddComponent<MeshCollider>();
+    }
+
+    void GenerateBlock(Vector2Int startCoords)
+    {
+        Vector2 topLeft, topRight, bottomLeft, bottomRight;
+
+        //top left corner
+        if(worldMap.ContainsKey(new Vector2Int(startCoords.x - 1, startCoords.y)))
+            topLeft = new Vector2
+            (
+                worldMap[new Vector2Int(startCoords.x - 1, startCoords.y)].topRight.x - (blockSize - roadSize), 
+                worldMap[new Vector2Int(startCoords.x - 1, startCoords.y)].topRight.y
+            );
+        else if(worldMap.ContainsKey(new Vector2Int(startCoords.x, startCoords.y + 1)))
+            topLeft = new Vector2
+            (
+                worldMap[new Vector2Int(startCoords.x, startCoords.y + 1)].bottomLeft.x, 
+                worldMap[new Vector2Int(startCoords.x, startCoords.y + 1)].bottomLeft.y + (blockSize - roadSize)
+            );
+        else
+            topLeft = new Vector2
+            (
+                - ((blockSize - roadSize) / 2) + UnityEngine.Random.Range(-variationAmount, variationAmount), 
+                ((blockSize - roadSize) / 2) + UnityEngine.Random.Range(-variationAmount, variationAmount)
+            );
+
+        //top right corner
+        if(worldMap.ContainsKey(new Vector2Int(startCoords.x + 1, startCoords.y)))
+            topRight = new Vector2
+            (
+                worldMap[new Vector2Int(startCoords.x + 1, startCoords.y)].topLeft.x + (blockSize - roadSize),
+                worldMap[new Vector2Int(startCoords.x + 1, startCoords.y)].topLeft.y
+            );
+        else if(worldMap.ContainsKey(new Vector2Int(startCoords.x, startCoords.y + 1)))
+            topRight = new Vector2
+            (
+                worldMap[new Vector2Int(startCoords.x, startCoords.y + 1)].bottomRight.x, 
+                worldMap[new Vector2Int(startCoords.x, startCoords.y + 1)].bottomRight.y + (blockSize - roadSize)
+            );
+        else
+            topRight = new Vector2
+            (
+                ((blockSize - roadSize) / 2) + UnityEngine.Random.Range(-variationAmount, variationAmount), 
+                ((blockSize - roadSize) / 2) + UnityEngine.Random.Range(-variationAmount, variationAmount)
+            );
+            
+        //bottom left corner
+        if(worldMap.ContainsKey(new Vector2Int(startCoords.x - 1, startCoords.y)))
+            bottomLeft = new Vector2
+            (
+                worldMap[new Vector2Int(startCoords.x - 1, startCoords.y)].bottomRight.x - (blockSize - roadSize), 
+                worldMap[new Vector2Int(startCoords.x - 1, startCoords.y)].bottomRight.y
+            );
+        else if(worldMap.ContainsKey(new Vector2Int(startCoords.x, startCoords.y - 1)))
+            bottomLeft = new Vector2
+            (
+                worldMap[new Vector2Int(startCoords.x, startCoords.y - 1)].topLeft.x, 
+                worldMap[new Vector2Int(startCoords.x, startCoords.y - 1)].topLeft.y - (blockSize - roadSize)
+            );
+        else
+            bottomLeft = new Vector2
+            (
+                - ((blockSize - roadSize) / 2) + UnityEngine.Random.Range(-variationAmount, variationAmount),
+                - ((blockSize - roadSize) / 2) + UnityEngine.Random.Range(-variationAmount, variationAmount)
+            );
+
+        //bottom right        
+        if(worldMap.ContainsKey(new Vector2Int(startCoords.x + 1, startCoords.y)))
+            bottomRight = new Vector2
+            (
+                worldMap[new Vector2Int(startCoords.x + 1, startCoords.y)].bottomLeft.x + (blockSize - roadSize), 
+                worldMap[new Vector2Int(startCoords.x + 1, startCoords.y)].bottomLeft.y
+                );
+        else if(worldMap.ContainsKey(new Vector2Int(startCoords.x, startCoords.y - 1)))
+            bottomRight = new Vector2
+            (
+                worldMap[new Vector2Int(startCoords.x, startCoords.y - 1)].topRight.x, 
+                worldMap[new Vector2Int(startCoords.x, startCoords.y - 1)].topRight.y - (blockSize - roadSize)
+            );
+        else
+            bottomRight = new Vector2
+            (
+                ((blockSize - roadSize) / 2) + UnityEngine.Random.Range(-variationAmount, variationAmount), 
+                - ((blockSize - roadSize) / 2) + UnityEngine.Random.Range(-variationAmount, variationAmount)
+            );
+        
+        float angle = Mathf.Atan2(bottomLeft.y - topLeft.y, bottomLeft.x - topLeft.x) -
+                Mathf.Atan2(topRight.y - topLeft.y, topRight.x - topLeft.x);
+        
+        print(angle);
+
+        Block block = new Block(topLeft, topRight, bottomLeft, bottomRight);
+        worldMap.Add(startCoords, block);
     }
 
     void LoadBlocks()
@@ -73,9 +234,13 @@ public class RoadGenerator : MonoBehaviour
 
                 if(IsInBounds(block, bounds) && !IsInBounds(block, loadedBounds))
                 {
-                    GameObject house = GameObject.Instantiate(housePrefab, new Vector3(block.x * blockSize, 0, block.y * blockSize), new Quaternion());
-                    house.name = VectToName(block);
-                    house.AddComponent<MeshCollider>();
+                    if(worldMap.ContainsKey(block))
+                        RenderBlock(block);
+                    else
+                    {
+                        GenerateBlock(block);
+                        RenderBlock(block);
+                    }
                 }
             }
 
@@ -106,7 +271,7 @@ public class RoadGenerator : MonoBehaviour
         bounds = new Vector2Int[4];
 
         Vector2Int currentBlock = new Vector2Int((int)(player.position.x / blockSize), (int)(player.position.z / blockSize));
-        print(currentBlock);
+        //print(currentBlock);
         
         bounds[0] = new Vector2Int(currentBlock.x - renderDistance, currentBlock.y + renderDistance);
         bounds[1] = new Vector2Int(currentBlock.x + renderDistance, currentBlock.y + renderDistance);
@@ -122,137 +287,4 @@ public class RoadGenerator : MonoBehaviour
         !bounds[2].Equals(loadedBounds[2]) ||
         !bounds[3].Equals(loadedBounds[3]);
     }
-
-    #region oldGenerator
-    /*const int gridWidth = 20, gridHeight = 20;
-    const float gridSpacing = 15f;
-    const float maxOffset = 6f;
-    const float gridStartX = -(gridWidth * gridSpacing) / 2;
-    const float gridStartY = -(gridHeight * gridSpacing) / 2;
-
-    Vector2Int[,] roadGrid = new Vector2Int[gridWidth, gridHeight];
-    List<Vector3> vertices = new List<Vector3>();
-    List<int> triangles = new List<int>();
-
-    void Start()
-    {
-        //Random.seed = System.DateTime.Now.GetHashCode();
-
-        PopulateGrid();
-        RenderRoad();
-    }
-
-    void RenderRoad()
-    {
-        int steps = 5;
-        int startingX = 10, startingY = 10;
-
-        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
-        meshRenderer.material = new Material(Shader.Find("Standard"));
-
-        Mesh mesh = new Mesh();
-
-        DrawCross(startingX, startingY);
-        for(int step = 1; step < steps; step++)
-        {
-            for(int pointN = 0; pointN < step + 1; pointN++)
-            {
-                //left section
-                if(startingX - step >= 0 && startingY - step + pointN * 2 >= 0)
-                    RenderCross(startingX - step, startingY - step + pointN * 2);
-
-                //right section
-                if(startingX + step < gridWidth && startingY - step + pointN * 2 >= 0)
-                    RenderCross(startingX + step, startingY - step + pointN * 2);
-
-                //down section
-                if(startingY - step >= 0 && startingX - step + pointN * 2 >= 0)
-                    RenderCross(startingX - step + pointN * 2, startingY - step);
-
-                //up section
-                if(startingY + step < gridHeight && startingX - step + pointN * 2 >= 0)
-                    RenderCross(startingX - step + pointN * 2, startingY + step);
-            }
-        }
-    }
-
-    void PopulateGrid()
-    {
-        for(int x = 0; x < gridWidth; x++)
-            for(int y = 0; y < gridHeight; y++)
-            {
-                roadGrid[x, y] = CalculateOffset();
-            }
-    }
-
-   Vector2Int CalculateOffset()
-    {
-        int direction = Random.Range(0, 360);
-
-        float offsetX = Mathf.Cos(Mathf.Deg2Rad * direction) * Random.Range(0, maxOffset);
-        float offsetY = Mathf.Sin(Mathf.Deg2Rad * direction) * Random.Range(0, maxOffset);
-
-        return new Vector2Int(offsetX, offsetY);
-    }
-
-    // void OnDrawGizmos()
-    // {
-
-    //     calculatedPoints = new List<string>();
-
-
-    //     for(int x = 0; x < gridWidth - 1; x++)
-    //         for(int y = 0; y < gridHeight - 1; y++)
-    //         {
-    //             float gridX = gridStartX + roadGrid[x, y].x + (x * gridSpacing);
-    //             float gridY = gridStartY + roadGrid[x, y].y + (y * gridSpacing);
-
-    //             Gizmos.DrawSphere(new Vector3(gridX, 0, gridY), 0.1f);
-    //         }
-    // }
-
-void RenderCross(int x, int y)
-{
-    //left
-    if(x - 1 >= 0)
-        RenderStake(GetVector(x, y), GetVector(x - 1, y));
-    //right
-    if(x + 1 < gridWidth)
-        RenderStake(GetVector(x, y), GetVector(x - 1, y));
-    //down
-    if(y - 1 >= 0)
-        RenderStake(GetVector(x, y), GetVector(x - 1, y));
-    //up
-    if(y + 1 < gridHeight)
-        RenderStake(GetVector(x, y), GetVector(x - 1, y));
-}
-
-void RenderStake(Vector3 from, Vector3 to)
-{
-}
-
-    void DrawCross(int x, int y)
-    {
-        //left
-        if(x - 1 >= 0)
-            Gizmos.DrawLine(GetVector(x, y), GetVector(x - 1, y));
-        //right
-        if(x + 1 < gridWidth)
-            Gizmos.DrawLine(GetVector(x, y), GetVector(x + 1, y));
-        //down
-        if(y - 1 >= 0)
-            Gizmos.DrawLine(GetVector(x, y), GetVector(x, y - 1));
-        //up
-        if(y + 1 < gridHeight)
-            Gizmos.DrawLine(GetVector(x, y), GetVector(x, y + 1));
-    }
-
-    Vector3 GetVector(int x, int y)
-    {
-        float gridX = gridStartX + roadGrid[x, y].x + (x * gridSpacing);
-        float gridY = gridStartY + roadGrid[x, y].y + (y * gridSpacing);
-
-        return new Vector3(gridX, 0, gridY);
-    }*/
-    #endregion oldGenerator
 }
